ace9fdc4c894ecd98da13d9c9dab42397c69006b
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-03-15 12:53:50 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import baseUrl from './config/baseUrl'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [pageNumber, setPageNumber] = useState(1);
+
   const fetchData = async (page = 0) => {
     const response = await Axios.post(`${baseUrl}/getByPage`, {
       page: page,
@@ -14,9 +16,13 @@ function App() {
     console.log(response.data)
   }
   useEffect(() => {
-    fetchData()
+    fetchData(pageNumber)
     console.log(products);
-  }, [])
+  }, [pageNumber])
+
+  const handlePageChange = (event) => {
+    setPageNumber(parseInt(event.target.value));
+  };
 
   return (
     <>
@@ -24,13 +30,19 @@ function App() {
         {
           products.map((product, i) => (
             <div key={i}>
-              <li>{product.ProductID}</li>
-              <li>{product.ProductName}</li>
+              <li>{product.ProductID} - {product.ProductName}</li>
             </div>
           ))
         }
       </ul>
-
+      <label htmlFor=""> Page number</label><br />
+      <input
+        type="number"
+        id="pageNumber"
+        min="1"
+        max="5"
+        value={pageNumber}
+        onChange={handlePageChange} />
     </>
   )
 }
